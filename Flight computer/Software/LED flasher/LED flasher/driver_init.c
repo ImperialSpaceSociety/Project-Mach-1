38934f0ecfd6207b0279b8342f70199d9229d64d
MedadRufus <medadrufus@gmail.com>
2019-11-07 21:41:06 +0000

    put in blink software

---
+++ Flight computer/Software/LED flasher/LED flasher/driver_init.c
@@ -0,0 +1,40 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PB30
+
+	gpio_set_pin_level(LED0,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(LED0, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(LED0, GPIO_PIN_FUNCTION_OFF);
+
+	delay_driver_init();
+}
--- Flight computer/Software/SAMD flight computer code/SAMD flight computer code/main.c
+++ Flight computer/Software/SAMD flight computer code/SAMD flight computer code/main.c
@@ -7,5 +7,6 @@ int main(void)
 
 	/* Replace with your application code */
 	while (1) {
+		
 	}
 }
