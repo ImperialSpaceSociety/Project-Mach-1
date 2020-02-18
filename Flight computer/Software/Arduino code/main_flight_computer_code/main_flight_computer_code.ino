b8c102f83e8ebe8d8ece56fa958e65689d5b6a3e
MedadRufus <medadrufus@gmail.com>
2020-02-18 20:37:36 +0000

    Put in main flight computer code folder

---
+++ Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
@@ -0,0 +1,15 @@
+#include "H3LIS100DL.h"
+#include "MS5607.h.h"
+#include "Ublox.h"
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
