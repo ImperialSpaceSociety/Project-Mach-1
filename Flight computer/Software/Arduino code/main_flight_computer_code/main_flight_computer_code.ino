7aa0f1532fc2ca748dbd8e777469d12fe253b974
MedadRufus <medadrufus@gmail.com>
2020-02-18 21:00:35 +0000

    put in code for the h3Lis100 into main

--- Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
+++ Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
@@ -1,15 +1,57 @@
+/* Code for the Rocket Flight computer
+ * Imperial College Space Society
+ * version : 1.0
+ * Date: 18/02/2020
+ * Currently the code only polls the H2LIS100 acclerometer and prints out the values.
+ * TODO: 
+ * 1. get all the other sensors to work and print out values
+ * 2. get the SPI flash chip to record down the values
+ * 3. Get the radio to transmit its position.
+ */
+
+
 #include "H3LIS100DL.h"
 #include "MS5607.h.h"
 #include "Ublox.h"
 
+#include <Wire.h>
+
+//please get these value by running H3LIS331DL_AdjVal Sketch.
+#define VAL_X_AXIS  203
+#define VAL_Y_AXIS  165
+#define VAL_Z_AXIS  141
+
+H3LIS331DL h3lis;
+
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  h3lis.init();
+  h3lis.importPara(VAL_X_AXIS,VAL_Y_AXIS,VAL_Z_AXIS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int16_t x,y,z;
+  h3lis.readXYZ(&x,&y,&z);
+  Serial.print("x, y, z = ");
+  Serial.print(x);
+  Serial.print("\t");
+  Serial.print(y);
+  Serial.print("\t");
+  Serial.println(z);
 
+  double xyz[3];
+  h3lis.getAcceleration(xyz);
+  Serial.print("accelerate of x, y, z = ");
+  Serial.print(xyz[0]);
+  Serial.print("g");
+  Serial.print("\t");
+  Serial.print(xyz[1]);
+  Serial.print("g");
+  Serial.print("\t");
+  Serial.print(xyz[2]);
+  Serial.println("g");  
+  
+  delay(1);
 }
