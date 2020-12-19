83c73c2eba1809f6c81f5f2d967532106f672e62
Adam Brzosko <47273267+ab7018@users.noreply.github.com>
2020-12-19 12:51:11 +0000

    radio

--- Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
+++ Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
@@ -15,8 +15,18 @@
 #include "MS5607.h"
 #include "Ublox.h"
 #include "SparkFunLSM9DS1.h"
+#include "Si446x.h"
 
 #include <Wire.h>
+
+///radio stuff
+#define CHANNEL 1
+#define MAX_PACKET_SIZE 500
+#define TIMEOUT 1000
+
+#define PACKET_NONE    0
+#define PACKET_OK   1
+#define PACKET_INVALID  2
 
 //please get these value by running H3LIS331DL_AdjVal Sketch.
 #define VAL_X_AXIS  203
@@ -28,9 +38,42 @@ SFE_UBLOX_GPS ubloxGps;
 MS5xxx ms5(&Wire);
 LSM9DS1 imu;
 
+typedef struct{
+  uint8_t ready;
+  uint32_t timestamp;
+  int16_t rssi;
+  uint8_t length;
+  uint8_t buffer[MAX_PACKET_SIZE];
+} pingInfo_t;
+
+static volatile pingInfo_t pingInfo;
+
+void SI446X_CB_RXCOMPLETE(uint8_t length, int16_t rssi)
+{
+  if(length > MAX_PACKET_SIZE)
+    length = MAX_PACKET_SIZE;
+
+  pingInfo.ready = PACKET_OK;
+  pingInfo.timestamp = millis();
+  pingInfo.rssi = rssi;
+  pingInfo.length = length;
+
+  Si446x_read((uint8_t*)pingInfo.buffer, length);
+
+  // Radio will now be in idle mode
+}
+
+void SI446X_CB_RXINVALID(int16_t rssi)
+{
+  pingInfo.ready = PACKET_INVALID;
+  pingInfo.rssi = rssi;
+}
+
 void setup() {
   Wire.begin();
   Serial.begin(9600);
+  Si446x_init();
+  Si446x_setTxPower(SI446X_MAX_TX_POWER);
   h3lis.init();
   if (!ubloxGps.begin())
   {
@@ -56,6 +99,19 @@ void loop() {
   int16_t x,y,z;
   long latitude, longitude, altitude;
   readGps(&latitude, &longitude, &altitude);
+  
+  //convert to string (not sure if neccessary but dont know how to do it differently)
+  char dataLat[MAX_PACKET_SIZE] = {0};
+  char dataLon[MAX_PACKET_SIZE] = {0};
+  char dataAlt[MAX_PACKET_SIZE] = {0};
+  sprintf_P(dataLat, "%ld", latitude);
+  sprintf_P(dataLon, "%ld", latitude);
+  sprintf_P(dataAlt, "%ld", latitude);
+  // Send the data (not sure if this works, maybe can do in one pulse?)
+  Si446x_TX(dataLat, sizeof(dataLat), CHANNEL, SI446X_STATE_RX);
+  Si446x_TX(dataLon, sizeof(dataLon), CHANNEL, SI446X_STATE_RX);
+  Si446x_TX(dataAlt, sizeof(dataAlt), CHANNEL, SI446X_STATE_RX);
+  
   h3lis.readXYZ(&x,&y,&z);
   Serial.print("x, y, z = ");
   Serial.print(x);
