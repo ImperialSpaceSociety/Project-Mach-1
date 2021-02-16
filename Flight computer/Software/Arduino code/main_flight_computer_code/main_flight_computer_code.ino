bc8c0c79c9ef1a119057375d5cf5efdd3c8b4b12
John <yaojohn6@gmail.com>
2021-02-16 17:36:09 +0000

    added stop after n seconds functionality, test results before run, and erase chip option

--- Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
+++ Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
@@ -14,7 +14,6 @@
 #include "H3LIS100DL.h"
 #include "MS5607.h"
 #include "Ublox.h"
-
 #include <Wire.h>
 #include <SPIMemory.h>
 
@@ -27,7 +26,8 @@ H3LIS331DL h3lis;
 MS5xxx ms5(&Wire);
 SPIFlash flash(SS_FLASH, &SPI1);
 
-long lasttime = 0;
+long starttime;
+long lasttime;
 
 struct dataPacket{
   //h3lis, location and accuracy
@@ -39,38 +39,122 @@ struct dataPacket{
   double pressure;
 };
 
+#define PACKET_SIZE sizeof(dataPacket)
+
+//the one datapacket reference we iterate on every run
 dataPacket dp;
+
+//starting address and cursor pointer
 uint32_t run_start;
 uint32_t _addr;
 
-#define PACKET_SIZE sizeof(dp)
+bool limit = false;
+int n;
 
 void setup() {
   Serial.begin(9600);
   h3lis.init();
   h3lis.importPara(VAL_X_AXIS,VAL_Y_AXIS,VAL_Z_AXIS);
   
+  if (flash.error()) {
+    Serial.println(flash.error(VERBOSE));
+  }
   flash.begin();
-  //erase chip before run
-  flash.eraseChip();
+
+  //test all functionalities on the flash chip, pauses for confirmation
+  delay(3000);
+  Serial.println("Running tests on flash....");
+  run_all_tests();
+  Serial.println("Tests complete. If all looks good, enter y to continue");
+
+  while(true) // remain here until told to break
+  {
+    if(Serial.available() > 0)
+      if(Serial.read() == 'y')
+        break;
+  }
+
+  Serial.println("Erase chip before run? enter y/n");
+  bool erase = false;
+  while(true) // remain here until told to break
+  {
+    if(Serial.available() > 0){
+      char res = Serial.read();
+      if(res == 'y' || res == 'n'){
+        erase = res == 'y';
+        break; 
+      } 
+    }
+  }
+  if(erase){
+    //erase chip before run
+    Serial.println("Erasing chip in preparation for run...");
+    flash.eraseChip();
+  }
+
+  Serial.println("Run for n seconds? y/n");
+  while(true) // remain here until told to break
+  {
+    if(Serial.available() > 0){
+      char res = Serial.read();
+      if(res == 'y' || res == 'n'){
+        limit = res == 'y';
+        break; 
+      } 
+    }
+  }
+  if(limit){
+    Serial.println("How many seconds to record for. Insert number:");
+    while(true) // remain here until told to break
+    {
+      if(Serial.available() > 0){
+        String st = Serial.readString();
+        if(st.toInt() > 0){
+           n = st.toInt();
+           break;
+        }
+      }
+    }
+  }
+  
+  
   //find suitable starting place for writing (should be 0)
   run_start = flash.getAddress(PACKET_SIZE);
   _addr = run_start;
-  Serial.print("=========================================");
-  Serial.print("This is the Rocket Flight Computer, v1.0");
-  Serial.print("=========================================");
+  Serial.println("=========================================");
+  Serial.println("This is the Rocket Flight Computer, v1.0");
+  Serial.println("Press 1 and enter at any time to dump results from flash and exit the main loop.");
+  Serial.println("=========================================");
+  starttime = millis();
   
 }
+uint8_t command = 0;
 
 void loop() {
   if(millis() - lasttime > 10){
+    if(limit && lasttime > (starttime + n * 1000)){
+      Serial.println(lasttime);
+      Serial.println(starttime);
+      Serial.println(n * 1000);
+      dumpFlash();
+      exit(0);
+    }
+    
     lasttime = millis(); //Update the timer
     read_info(&dp);
     //print_info(&dp);
-    Serial.println(flash.getCapacity());
     write_info(dp);
-    read_from_flash();
     Serial.println(_addr);
+
+    if (command == 1){
+      dumpFlash();
+      exit(0);
+      command = 0;
+    }
+  }
+
+  if (Serial.available() > 0){
+    command = Serial.parseInt();      
   }
 }
 
@@ -88,9 +172,9 @@ void read_info(dataPacket *dp){
   dp->pressure = ms5.GetPres();
 }
 
-void read_from_flash(){
+void read_from_flash(uint32_t addr){
   dataPacket out;
-  flash.readAnything(_addr - PACKET_SIZE, out);
+  flash.readAnything(addr - PACKET_SIZE, out);
   print_info(&out);
 }
 
@@ -133,33 +217,100 @@ bool write_info(dataPacket dp){
 }
 
 void dumpFlash(){
-  Serial.println("Reading all pages");
-  uint8_t data_buffer[256];
-
-  uint32_t maxAddr = flash.getCapacity();
-  for (int a = 0; a < maxAddr; a++) {
-    flash.readByteArray(a, &data_buffer[0], 256);
-    _printPageBytes(data_buffer, 1);
-    delay(100);
-  }  
+  int num_packets = _addr / PACKET_SIZE;
+  for(int i = 1; i < num_packets; i++){
+    read_from_flash(run_start + i * PACKET_SIZE);
+  }
 }
 
-void _printPageBytes(uint8_t *data_buffer, uint8_t outputType) {
-  char buffer[10];
-  for (int a = 0; a < 16; ++a) {
-    for (int b = 0; b < 16; ++b) {
-      if (outputType == 1) {
-        sprintf(buffer, "%02x", data_buffer[a * 16 + b]);
-        Serial.print(buffer);
-      }
-      else if (outputType == 2) {
-        uint8_t x = data_buffer[a * 16 + b];
-        if (x < 10) Serial.print("0");
-        if (x < 100) Serial.print("0");
-        Serial.print(x);
-        Serial.print(',');
-      }
-    }
-    Serial.println();
+void run_all_tests(){
+  printLine();
+  printTab(7);
+  Serial.print("Testing library code");
+  printLine();
+  printTab(3);
+  Serial.print("Function");
+  printTab(2);
+  Serial.print("Test result");
+  printTab(3);
+  Serial.print("     Runtime");
+  printLine();
+
+  powerDownTest();
+  Serial.println();
+  powerUpTest();
+  Serial.println();
+
+  Serial.println();
+
+  eraseChipTest();
+  Serial.println();
+  eraseSectionTest();
+  Serial.println();
+  eraseBlock64KTest();
+  Serial.println();
+  eraseBlock32KTest();
+  Serial.println();
+  eraseSectorTest();
+  //Serial.println();
+
+
+  /* #if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+      delay(10);
+      powerDownTest();
+      powerUpTest();
+      Serial.println();
+    #endif */
+  printLine();
+  printTab(3);
+  Serial.print("Data type");
+  printTab(2);
+  Serial.print("I/O Result");
+  printTab(1);
+  Serial.print("      Write time");
+  printTab(1);
+  Serial.print("      Read time");
+  printLine();
+
+  byteTest();
+  Serial.println();
+  charTest();
+  Serial.println();
+  wordTest();
+  Serial.println();
+  shortTest();
+  Serial.println();
+  uLongTest();
+  Serial.println();
+  /*#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+      delay(10);
+    #endif */
+  longTest();
+  Serial.println();
+  floatTest();
+  Serial.println();
+  structTest();
+  Serial.println();
+  arrayTest();
+  Serial.println();
+  stringTest();
+
+  /* #if !defined(ARDUINO_ARCH_SAM) || !defined(ARDUINO_ARCH_ESP8266)
+      Serial.println();
+      powerDownTest();
+      powerUpTest();
+    #endif */
+  printLine();
+  if (!flash.functionRunTime()) {
+    Serial.println(F("To see function runtimes ncomment RUNDIAGNOSTIC in SPIMemory.h."));
   }
+}
+
+int isNumeric (const char *s)
+{
+    if (s == NULL || *s == '\0' || isspace(*s))
+      return 0;
+    char * p;
+    strtod (s, &p);
+    return *p == '\0';
 }
