625ec47a530eebfe8e50d9bb00946bd3a0c7b0a3
John <yaojohn6@gmail.com>
2021-02-23 01:35:07 +0000

    cleaned up code, made sure it works

--- Flight computer/Software/Arduino code/main_flight_computer_code/flash_test_functions.ino
+++ Flight computer/Software/Arduino code/main_flight_computer_code/flash_test_functions.ino
@@ -17,6 +17,91 @@
 #define TRUE 1
 #define FALSE 0
 
+void run_all_tests(){
+  printLine();
+  printTab(7);
+  Serial.print("Testing library code");
+  printLine();
+  printTab(3);
+  Serial.print("Function");
+  printTab(2);
+  Serial.print("Test result");
+  printTab(3);
+  Serial.print("     Runtime");
+  printLine();
+
+  powerDownTest();
+  Serial.println();
+  powerUpTest();
+  Serial.println();
+
+  Serial.println();
+
+  eraseChipTest();
+  Serial.println();
+  eraseSectionTest();
+  Serial.println();
+  eraseBlock64KTest();
+  Serial.println();
+  eraseBlock32KTest();
+  Serial.println();
+  eraseSectorTest();
+  //Serial.println();
+
+
+  /* #if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+      delay(10);
+      powerDownTest();
+      powerUpTest();
+      Serial.println();
+    #endif */
+  printLine();
+  printTab(3);
+  Serial.print("Data type");
+  printTab(2);
+  Serial.print("I/O Result");
+  printTab(1);
+  Serial.print("      Write time");
+  printTab(1);
+  Serial.print("      Read time");
+  printLine();
+
+  byteTest();
+  Serial.println();
+  charTest();
+  Serial.println();
+  wordTest();
+  Serial.println();
+  shortTest();
+  Serial.println();
+  uLongTest();
+  Serial.println();
+  /*#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+      delay(10);
+    #endif */
+  longTest();
+  Serial.println();
+  floatTest();
+  Serial.println();
+  structTest();
+  Serial.println();
+  arrayTest();
+  Serial.println();
+  stringTest();
+
+  /* #if !defined(ARDUINO_ARCH_SAM) || !defined(ARDUINO_ARCH_ESP8266)
+      Serial.println();
+      powerDownTest();
+      powerUpTest();
+    #endif */
+  printLine();
+  if (!flash.functionRunTime()) {
+    Serial.println(F("To see function runtimes ncomment RUNDIAGNOSTIC in SPIMemory.h."));
+  }
+}
+
+
+
 void printLine() {
   Serial.println();
   for (uint8_t i = 0; i < 125; i++) {
--- Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
+++ Flight computer/Software/Arduino code/main_flight_computer_code/main_flight_computer_code.ino
@@ -40,12 +40,16 @@
 #define VAL_Y_AXIS  165
 #define VAL_Z_AXIS  141
 
+//sensor objects
 H3LIS331DL h3lis;
 SFE_UBLOX_GPS ubloxGps;
+LSM9DS1 imu;
 MS5xxx ms5(&Wire);
 
+//flash
 SPIFlash flash(SS_FLASH, &SPI1);
 
+//time-sensitive util variables
 long starttime;
 long lasttime;
 
@@ -57,6 +61,16 @@ struct dataPacket{
   //m5 temp + pressure
   double temp;
   double pressure;
+
+  //GPS latitude, longitude, altitude
+  long latitude; 
+  long longitude;
+  long altitude;
+
+  //imu gyro, mag, alt, temp
+  double gyro[3];
+  double accel[3];
+  double mag[3];
 };
 
 #define PACKET_SIZE sizeof(dataPacket)
@@ -68,9 +82,10 @@ dataPacket dp;
 uint32_t run_start;
 uint32_t _addr;
 
+//utility variables
 bool limit = false;
+uint8_t command = 0;
 int n;
-LSM9DS1 imu;
 
 struct pingInfo_t{
   uint8_t ready;
@@ -82,29 +97,9 @@ struct pingInfo_t{
 
 static volatile pingInfo_t pingInfo;
 
+//Why do we need these???
 static uint8_t tx_str_buffer[MAX_PACKET_SIZE];
 void fill_tx_buffer_with_location(uint16_t start_point, uint8_t * buffer, uint16_t latitude, uint16_t longitude, uint16_t altitude );
-
-void SI446X_CB_RXCOMPLETE(uint8_t length, int16_t rssi)
-{
-  if(length > MAX_PACKET_SIZE)
-    length = MAX_PACKET_SIZE;
-
-  pingInfo.ready = PACKET_OK;
-  pingInfo.timestamp = millis();
-  pingInfo.rssi = rssi;
-  pingInfo.length = length;
-
-  Si446x_read((uint8_t*)pingInfo.buffer, length);
-
-  // Radio will now be in idle mode
-}
-
-void SI446X_CB_RXINVALID(int16_t rssi)
-{
-  pingInfo.ready = PACKET_INVALID;
-  pingInfo.rssi = rssi;
-}
 
 void setup() {
   Wire.begin();
@@ -113,6 +108,7 @@ void setup() {
   Si446x_setTxPower(SI446X_MAX_TX_POWER);
   h3lis.init();
   h3lis.importPara(VAL_X_AXIS,VAL_Y_AXIS,VAL_Z_AXIS);
+  ubloxGps.setI2COutput(COM_TYPE_UBX);
   
   if (flash.error()) {
     Serial.println(flash.error(VERBOSE));
@@ -125,11 +121,8 @@ void setup() {
     while (1);
   }
   
-  ubloxGps.setI2COutput(COM_TYPE_UBX);
-  h3lis.importPara(VAL_X_AXIS,VAL_Y_AXIS,VAL_Z_AXIS); //3deg Accelerometer
   imu.begin();
   //imu.settings.device.commInterface = IMU_MODE_I2C;
-
 
   //test all functionalities on the flash chip, pauses for confirmation
   delay(3000);
@@ -187,7 +180,6 @@ void setup() {
     }
   }
   
-  
   //find suitable starting place for writing (should be 0)
   run_start = flash.getAddress(PACKET_SIZE);
   _addr = run_start;
@@ -197,10 +189,6 @@ void setup() {
   Serial.println("=========================================");
   starttime = millis();
 }
-
-
-
-uint8_t command = 0;
 
 void loop() { 
   
@@ -222,68 +210,8 @@ void loop() {
       exit(0);
       command = 0;
     }
-    
-    //TODO: fix this into the read_data function
-    int16_t x,y,z;
-    long latitude, longitude, altitude;
-    readGps(&latitude, &longitude, &altitude);
-    h3lis.readXYZ(&x,&y,&z);
-    Serial.print("x, y, z = ");
-    Serial.print(x);
-    Serial.print("\t");
-    Serial.print(y);
-    Serial.print("\t");
-    Serial.println(z);
 
-    double xyz[3];
-    h3lis.getAcceleration(xyz);
-    Serial.print("accelerate of x, y, z = ");
-    Serial.print(xyz[0]);
-    Serial.print("g");
-    Serial.print("\t");
-    Serial.print(xyz[1]);
-    Serial.print("g");
-    Serial.print("\t");
-    Serial.print(xyz[2]);
-    Serial.println("g");
-    
-    imu.readGyro();
-    imu.readAccel();
-    imu.readMag();
-    imu.readTemp();
-    Serial.print(imu.calcGyro(imu.gx), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcGyro(imu.gy), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcGyro(imu.gz), 2);
-    Serial.println(" deg/s");
-    Serial.print(imu.calcAccel(imu.ax), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcAccel(imu.ay), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcAccel(imu.az), 2);
-    Serial.println(" g");
-    Serial.print(imu.calcMag(imu.mx), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcMag(imu.my), 2);
-    Serial.print(", ");
-    Serial.print(imu.calcMag(imu.mz), 2);
-    Serial.println(" gauss");
-    Serial.print("9degTemperature: ");
-    Serial.println(imu.temperature);
-
-    ms5.ReadProm();
-    ms5.Readout();
-
-    dp.temp = ms5.GetTemp();
-    dp.pressure = ms5.GetPres();    
-
-    //convert to string (not sure if neccessary but dont know how to do it differently)
-    String data = String(latitude) + "," + String(longitude) + "," + String(altitude); 
-    Serial.print(data);
-    // Send the data
-    // TODO: send the only every second, not every 10 milliseconds.
-    Si446x_TX(&data, sizeof(data), CHANNEL, SI446X_STATE_RX);
+    radio_send_data(&dp);
   }
 
   if (Serial.available() > 0){
@@ -297,41 +225,37 @@ void loop() {
 }
 
 
-
-void manage_radio()
-{
-
-  char commands []= {'0','1','3'};
-
-  if(Serial.available()){
-    String command = Serial.readStringUntil('\n');
-    if(command.equals("Fire")){
-      Si446x_TX(&commands[0], sizeof(char), CHANNEL, SI446X_STATE_RX);
-    }
-    else if(command.equals("Clear")){
-      Si446x_TX(&commands[1], sizeof(char), CHANNEL, SI446X_STATE_RX);
-    }
-    else if(command.equals("Parachute")){
-      Si446x_TX(&commands[2], sizeof(char), CHANNEL, SI446X_STATE_RX);
-    }
-    else{
-      Serial.println("Invalid command");
-    }
-  }
-}
-
-
 //read info into a datapacket
 void read_info(dataPacket *dp){
   //TODO: Add try-catch around the data collections, return a dummy value if failed.
   h3lis.readXYZ(&(dp->location[0]),&(dp->location[1]),&(dp->location[2]));
   h3lis.getAcceleration(dp->acc);
-}
 
-void read_from_flash(uint32_t addr){
-  dataPacket out;
-  flash.readAnything(addr - PACKET_SIZE, out);
-  print_info(&out);
+  readGps(&(dp->latitude), &(dp->longitude), &(dp->altitude));
+
+  ms5.ReadProm();
+  ms5.Readout();
+
+  dp->temp = ms5.GetTemp();
+  dp->pressure = ms5.GetPres();
+
+  imu.readGyro();
+  imu.readAccel();
+  imu.readMag();
+  imu.readTemp();
+
+  dp->gyro[0] = imu.calcGyro(imu.gx);
+  dp->gyro[1] = imu.calcGyro(imu.gy);
+  dp->gyro[2] = imu.calcGyro(imu.gz);
+
+  dp->accel[0] = imu.calcGyro(imu.ax);
+  dp->accel[1] = imu.calcGyro(imu.ay);
+  dp->accel[2] = imu.calcGyro(imu.az);
+
+  dp->mag[0] = imu.calcGyro(imu.mx);
+  dp->mag[1] = imu.calcGyro(imu.my);
+  dp->mag[2] = imu.calcGyro(imu.mz);
+  
 }
 
 //print to serial port
@@ -357,124 +281,34 @@ void print_info(dataPacket *dp) {
     Serial.println(dp->temp);
     Serial.print("Pressure: ");
     Serial.println(dp->pressure);
-}
 
-bool write_info(dataPacket dp){
-  if (flash.writeAnything(_addr, dp)){
-    //increment address pointer
-    _addr += PACKET_SIZE;
+    Serial.print("Gyro: ");
+    Serial.print(dp->gyro[0], 2);
+    Serial.print(", ");
+    Serial.print(dp->gyro[1], 2);
+    Serial.print(", ");
+    Serial.print(dp->gyro[2], 2);
+    Serial.println(" deg/s");
+    Serial.print("Acceleration: ");
+    Serial.print(dp->accel[0], 2);
+    Serial.print(", ");
+    Serial.print(dp->accel[1], 2);
+    Serial.print(", ");
+    Serial.print(dp->accel[2], 2);
+    Serial.println(" g");
+    Serial.print("Mag: ");
+    Serial.print(dp->mag[0], 2);
+    Serial.print(", ");
+    Serial.print(dp->mag[1], 2);
+    Serial.print(", ");
+    Serial.print(dp->mag[2], 2);
+    Serial.println(" gauss");
 
-    return true;
-  }
-  else{
-    Serial.print("Problem writing to flash.");
-    return false;
-  }
-}
-
-void dumpFlash(){
-  int num_packets = _addr / PACKET_SIZE;
-  for(int i = 1; i < num_packets; i++){
-    read_from_flash(run_start + i * PACKET_SIZE);
-  }
-}
-
-void run_all_tests(){
-  printLine();
-  printTab(7);
-  Serial.print("Testing library code");
-  printLine();
-  printTab(3);
-  Serial.print("Function");
-  printTab(2);
-  Serial.print("Test result");
-  printTab(3);
-  Serial.print("     Runtime");
-  printLine();
-
-  powerDownTest();
-  Serial.println();
-  powerUpTest();
-  Serial.println();
-
-  Serial.println();
-
-  eraseChipTest();
-  Serial.println();
-  eraseSectionTest();
-  Serial.println();
-  eraseBlock64KTest();
-  Serial.println();
-  eraseBlock32KTest();
-  Serial.println();
-  eraseSectorTest();
-  //Serial.println();
-
-
-  /* #if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
-      delay(10);
-      powerDownTest();
-      powerUpTest();
-      Serial.println();
-    #endif */
-  printLine();
-  printTab(3);
-  Serial.print("Data type");
-  printTab(2);
-  Serial.print("I/O Result");
-  printTab(1);
-  Serial.print("      Write time");
-  printTab(1);
-  Serial.print("      Read time");
-  printLine();
-
-  byteTest();
-  Serial.println();
-  charTest();
-  Serial.println();
-  wordTest();
-  Serial.println();
-  shortTest();
-  Serial.println();
-  uLongTest();
-  Serial.println();
-  /*#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
-      delay(10);
-    #endif */
-  longTest();
-  Serial.println();
-  floatTest();
-  Serial.println();
-  structTest();
-  Serial.println();
-  arrayTest();
-  Serial.println();
-  stringTest();
-
-  /* #if !defined(ARDUINO_ARCH_SAM) || !defined(ARDUINO_ARCH_ESP8266)
-      Serial.println();
-      powerDownTest();
-      powerUpTest();
-    #endif */
-  printLine();
-  if (!flash.functionRunTime()) {
-    Serial.println(F("To see function runtimes ncomment RUNDIAGNOSTIC in SPIMemory.h."));
-  }
-}
-
-int isNumeric (const char *s)
-{
-    if (s == NULL || *s == '\0' || isspace(*s))
-      return 0;
-    char * p;
-    strtod (s, &p);
-    return *p == '\0';
-}
-
-
-void readGps(long *latitude, long *longitude, long *altitude) {
-  *latitude = ubloxGps.getLatitude();
-  *longitude = ubloxGps.getLongitude();
-  *altitude = ubloxGps.getAltitude();
-
+    Serial.print("GPS info: ");
+    Serial.print(dp->longitude);
+    Serial.print(", ");
+    Serial.print(dp->latitude);
+    Serial.print(", ");
+    Serial.print(dp->altitude);
+    Serial.println();
 }
