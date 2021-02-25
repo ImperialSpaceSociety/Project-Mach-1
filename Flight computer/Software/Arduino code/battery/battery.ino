7a07846f2678e05cec2f53c051ac6f448f220765
kubajed <47507183+kubajed@users.noreply.github.com>
2021-02-25 21:07:47 +0100

    Battery level reading
    Created a function reading the voltage level, it also calculates percentage of the battery given min and max voltages

---
+++ Flight computer/Software/Arduino code/battery/battery.ino
@@ -0,0 +1,31 @@
+int value = 0;
+float voltage;
+float percentage;
+
+#define MIN_BATT 3.6
+#define MAX_BATT 4.2
+
+void setup()
+{
+  analogReadResolution(12);
+}
+
+void loop(){
+
+BattLevel(&voltage, &percentage);
+
+}
+
+void BattLevel(float *v, float *p)
+{
+  value = analogRead(A2);
+  *v = value * 3.3/4095 * 2;
+  if(*v > MAX_BATT or *v < MIN_BATT)
+  {
+    *p = -1;
+  }
+  else
+  {
+    *p = *v * (100 / (MAX_BATT - MIN_BATT));
+  }
+}
