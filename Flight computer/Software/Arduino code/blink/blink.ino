3c836a16d54509506529783bc193434ca0d8f7e8
MedadRufus <medadrufus@gmail.com>
2021-06-17 23:22:09 +0100

    update comment

--- Flight computer/Software/Arduino code/blink/blink.ino
+++ Flight computer/Software/Arduino code/blink/blink.ino
@@ -15,8 +15,9 @@ void setup() {
   // initialize digital pins as output.
   pinMode(PIN_LED_RED, OUTPUT);
   pinMode(PIN_LED_BLUE, OUTPUT);
-}// the loop function runs over and over again forever
+}
 
+// the loop function runs over and over again forever
 void loop() {
   digitalWrite(PIN_LED_RED, HIGH); // turn the LED on (HIGH is the voltage level)
   digitalWrite(PIN_LED_BLUE, LOW); // turn the LED off by making the voltage LOW
