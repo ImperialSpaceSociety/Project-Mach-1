ad3a345f3e2871386c435d675bd0fda0bf01efdc
MedadRufus <medadrufus@gmail.com>
2021-06-18 02:28:17 +0100

    format and import

--- Flight computer/Software/Arduino code/rocket_flight_computer_platformio/src/flash_functions.cpp
+++ Flight computer/Software/Arduino code/rocket_flight_computer_platformio/src/flash_functions.cpp
@@ -1,26 +1,38 @@
+#include "Arduino.h"
+#include "datapacket.hpp"
 
-void read_from_flash(uint32_t addr){
+//starting address and cursor pointer
+uint32_t run_start;
+uint32_t _addr;
+
+void read_from_flash(uint32_t addr)
+{
   dataPacket out;
   flash.readAnything(addr - PACKET_SIZE, out);
   print_info(&out);
 }
 
-bool write_info(dataPacket dp){
-  if (flash.writeAnything(_addr, dp)){
+bool write_info(dataPacket dp)
+{
+  if (flash.writeAnything(_addr, dp))
+  {
     //increment address pointer
     _addr += PACKET_SIZE;
 
     return true;
   }
-  else{
+  else
+  {
     Serial.print("Problem writing to flash.");
     return false;
   }
 }
 
-void dumpFlash(){
+void dumpFlash()
+{
   int num_packets = _addr / PACKET_SIZE;
-  for(int i = 1; i < num_packets; i++){
+  for (int i = 1; i < num_packets; i++)
+  {
     read_from_flash(run_start + i * PACKET_SIZE);
   }
 }
