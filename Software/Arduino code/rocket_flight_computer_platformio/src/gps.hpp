2bf7616093de7e92899eea86e07565da404bc43d
MedadRufus <medadrufus@gmail.com>
2021-06-22 00:32:51 +0100

    put in ublox callback code

---
+++ Software/Arduino code/rocket_flight_computer_platformio/src/gps.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file gps.hpp
+ * @author Medad Rufus Newman (mailto@medadnewman.co.uk)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+void gps_check();
+void gps_init();
