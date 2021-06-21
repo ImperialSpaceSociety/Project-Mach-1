e979b759e1bd1a701efd0c1b145ada772478c315
MedadRufus <medadrufus@gmail.com>
2021-06-21 23:14:38 +0100

    add in buzzer

--- Software/Arduino code/blink/blink.ino
+++ Software/Arduino code/blink/blink.ino
@@ -10,19 +10,29 @@
 #define PIN_LED_BLUE (39u)
 #endif
 
+#ifndef PIN_BUZZER
+#define PIN_BUZZER (0u)
+#endif
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pins as output.
   pinMode(PIN_LED_RED, OUTPUT);
   pinMode(PIN_LED_BLUE, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(PIN_LED_RED, HIGH); // turn the LED on (HIGH is the voltage level)
   digitalWrite(PIN_LED_BLUE, LOW); // turn the LED off by making the voltage LOW
+  tone(PIN_BUZZER, 1000); // Send 1KHz sound signal...
+  
   delay(200); // wait for a second
   digitalWrite(PIN_LED_RED, LOW); // turn the LED off by making the voltage LOW
   digitalWrite(PIN_LED_BLUE, HIGH); // turn the LED on (HIGH is the voltage level)
+  noTone(PIN_BUZZER);     // Stop sound...
+  
   delay(500); // wait for half a second
 }
