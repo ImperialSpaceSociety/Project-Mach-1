31616a7a8c6dfec38332acb8908f928d5e603169
MedadRufus <medadrufus@gmail.com>
2021-06-25 22:20:30 +0100

    allow sensor read

--- Software/Arduino code/rocket_flight_computer_platformio/src/main.cpp
+++ Software/Arduino code/rocket_flight_computer_platformio/src/main.cpp
@@ -460,7 +460,7 @@ void setup_rtos()
   // Create the threads that will be managed by the rtos
   // Sets the stack size and priority of each task
   // Also initializes a handler pointer to each task, which are important to communicate with and retrieve info from tasks
-  //xTaskCreate(threadSensorRead, "threadSensorRead", 2000, NULL, tskIDLE_PRIORITY + 3, &Handle_SensorRead);
+  xTaskCreate(threadSensorRead, "threadSensorRead", 2000, NULL, tskIDLE_PRIORITY + 1, &Handle_SensorRead);
   xTaskCreate(TaskTelemetry, "TaskTelemetry", 256, NULL, tskIDLE_PRIORITY + 2, &Handle_TaskTelemetry);
   xTaskCreate(TaskBlink, "TaskBlink", 256, NULL, tskIDLE_PRIORITY + 2, &Handle_TaskBlink);
   xTaskCreate(taskMonitor, "Task Monitor", 256, NULL, tskIDLE_PRIORITY + 1, &Handle_monitorTask);
