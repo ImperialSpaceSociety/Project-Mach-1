015d3b477c47aa02550f0ad3dfa165a8476fb8bd
MedadRufus <medadrufus@gmail.com>
2021-06-26 01:11:36 +0100

    use adafruit spiflash for datastorage

--- main_code/src/main.cpp
+++ main_code/src/main.cpp
@@ -31,7 +31,10 @@
 #include "Si446x.h"
 #include <FreeRTOS_SAMD21.h>
 
-#include "file_system.hpp"
+#include <SPI.h>
+#include <Adafruit_SPIFlash.h>
+#include <Adafruit_SPIFlash_FatFs.h>
+
 #include "datapacket.hpp"
 #include "telemetry.hpp"
 #include "util.hpp"
@@ -46,6 +49,34 @@
 // Some boards use SerialUSB, some use Serial
 //#define SERIAL SerialUSB //Sparkfun Samd21 Boards
 #define SERIAL Serial //Adafruit, other Samd21 Boards
+
+// Configuration of the flash chip pins and flash fatfs object.
+// You don't normally need to change these if using a Feather/Metro
+// M0 express board.
+#define FLASH_TYPE SPIFLASHTYPE_W25Q64 // Flash chip type.         \
+                                       // If you change this be    \
+                                       // sure to change the fatfs \
+                                       // object type below to match.
+
+#if defined(__SAMD51__)
+// Alternatively you can define and use non-SPI pins, QSPI isnt on a sercom
+Adafruit_SPIFlash flash(PIN_QSPI_SCK, PIN_QSPI_IO1, PIN_QSPI_IO0, PIN_QSPI_CS);
+#else
+#if (SPI_INTERFACES_COUNT == 1)
+#define FLASH_SS SS        // Flash chip SS pin.
+#define FLASH_SPI_PORT SPI // What SPI port is Flash on?
+#else
+#define FLASH_SS SS1        // Flash chip SS pin.
+#define FLASH_SPI_PORT SPI1 // What SPI port is Flash on?
+#endif
+
+Adafruit_SPIFlash flash(FLASH_SS, &FLASH_SPI_PORT); // Use hardware SPI
+#endif
+
+Adafruit_W25Q16BV_FatFs fatfs(flash);
+
+// Configuration for the datalogging file:
+#define FILE_NAME "data.csv"
 
 //**************************************************************************
 // global variables
@@ -66,7 +97,6 @@ long lasttime;
 
 //the one datapacket reference we iterate on every run
 dataPacket_t dp;
-OurFile ourfile;
 
 typedef struct
 {
@@ -279,6 +309,29 @@ static void threadSensorRead(void *pvParameters)
   TickType_t xLastWakeTime;
   const TickType_t xFrequency = INA226_SAMPLE_INTERVAL;
 
+  // Initialize flash library and check its chip ID.
+  if (!flash.begin(FLASH_TYPE))
+  {
+    Serial.println("Error, failed to initialize flash chip!");
+    while (1)
+      ;
+  }
+  Serial.print("Flash chip JEDEC ID: 0x");
+  Serial.println(flash.GetJEDECID(), HEX);
+
+  // First call begin to mount the filesystem.  Check that it returns true
+  // to make sure the filesystem was mounted.
+  if (!fatfs.begin())
+  {
+    Serial.println("Error, failed to mount newly formatted filesystem!");
+    Serial.println("Was the flash chip formatted with the fatfs_format example?");
+    // while (1)
+    //   ;
+  }
+  Serial.println("Mounted filesystem!");
+
+  Serial.println("Logging data every 60 seconds...");
+
   // Initialise the xLastWakeTime variable with the current time.
   xLastWakeTime = xTaskGetTickCount();
   for (;;)
@@ -291,6 +344,26 @@ static void threadSensorRead(void *pvParameters)
     print_info(&dp);
 
     Serial.println("Writing ...");
+
+    // Open the datalogging file for writing.  The FILE_WRITE mode will open
+    // the file for appending, i.e. it will add new data to the end of the file.
+    Adafruit_SPIFlash_FAT::File dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
+    // Check that the file opened successfully and write a line to it.
+    if (dataFile)
+    {
+      tx_data.count++;
+      dataFile.write((const uint8_t *)&tx_data, sizeof(data_t));
+      // Finally close the file when done writing.  This is smart to do to make
+      // sure all the data is written to the file.
+      dataFile.close();
+      Serial.println("Wrote new measurement to data file!");
+    }
+    else
+    {
+      Serial.println("Failed to open data file for writing!");
+    }
+
+    Serial.println("Trying again in 60 seconds...");
   }
 }
 
