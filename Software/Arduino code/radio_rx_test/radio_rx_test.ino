445d3a673a9b43c38e272097dc5f4169a30f4276
MedadRufus <medadrufus@gmail.com>
2021-06-26 04:07:34 +0100

    lat before long

--- Software/Arduino code/radio_rx_test/radio_rx_test.ino
+++ Software/Arduino code/radio_rx_test/radio_rx_test.ino
@@ -128,10 +128,10 @@ void loop()
     data_t* msg = (data_t*)pingInfo.buffer;
     Serial.print(" Count= ");
     Serial.print(msg->count);
-    Serial.print(" Longitude= ");
-    Serial.print(msg->longitude/1e7,8);
     Serial.print(" Latitude= ");
     Serial.print(msg->latitude/1e7,8);
+    Serial.print(" Longitude= ");
+    Serial.print(msg->longitude/1e7,8);
     Serial.print(" Altitude= ");
     Serial.print(msg->altitude/1e3);
     Serial.println();
